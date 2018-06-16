a98e475c7091867680ac0a46ebe51b67bf51b7a7
Felice Forby <feliceforby@gmail.com>
2018-06-16 00:22:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,113 @@
+require 'pry'
+
+class Dog 
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(name:, breed:, id: nil)
+    @name = name
+    @breed = breed
+    @id = id
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      );
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS dogs;
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    if @id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs
+        (name, breed)
+        VALUES (?, ?);
+      SQL
+      
+      DB[:conn].execute(sql, @name, @breed)
+      
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs;")[0][0]
+      self
+    end
+  end
+  
+  def self.create(name:, breed:)
+    dog = self.new(name: name, breed: breed)
+    dog.save
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    
+    self.new(name: name, breed: breed, id: id)
+  end
+  
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE id = ?;
+    SQL
+    
+    dog_data = DB[:conn].execute(sql, id).first
+    self.new_from_db(dog_data)
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?;
+    SQL
+    
+    dog_data = DB[:conn].execute(sql, name).first
+    self.new_from_db(dog_data)
+  end
+  
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      AND breed = ?;
+    SQL
+    
+    # Check if dog exists with the given name and breed
+    dog_data = DB[:conn].execute(sql, name, breed).first
+    
+    if dog_data
+      self.new_from_db(dog_data)
+    else
+      self.create(name: name, breed: breed)
+    end
+  end
+  
+  def update
+    sql = <<-SQL
+      UPDATE dogs
+      SET name = ?,
+          breed = ?
+      WHERE id = ?;
+    SQL
+    
+    DB[:conn].execute(sql, @name, @breed, @id)
+  end
+end
